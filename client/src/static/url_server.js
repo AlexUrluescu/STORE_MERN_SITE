47f1b80dce803c071b8017e5d99a45298ad93e6d
AlexUrluescu <urluescualex2003@gmail.com>
2023-05-30 23:40:40 +0300

    Change the route for cloud

--- client/src/static/url_server.js
+++ client/src/static/url_server.js
@@ -1,4 +1,4 @@
 
 
-// export const url = "https://storeweb.onrender.com";
-export const url = "http://localhost:5000";
+export const url = "https://storeweb.onrender.com";
+// export const url = "http://localhost:5000";
