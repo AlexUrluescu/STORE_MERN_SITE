b2e739d25e7c89a527808203f5f109a3c1319f69
AlexUrluescu <urluescualex2003@gmail.com>
2023-05-31 07:08:52 +0300

    Fix an h1

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -67,7 +67,7 @@ const Login = ({setUserLogin}) => {
             <div className="container-fr">
                 <div className="login_container">
                     <div className="form_text">
-                        <h1><b>Welcome in StoreWeb</b></h1>
+                        <h1><b>Welcome in Babii Nails</b></h1>
                         <h3>Login with your email and password</h3>
                     </div>
                     <form onSubmit={handleSubmit} className="login_form">
